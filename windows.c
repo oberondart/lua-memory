039cd0a1c999173bce276be2c7ecfdc1db1bb395
devenoch <durustein@gmail.com>
2026-04-28 16:28:09 -0400

    Separating windows.c from pointer.c

--- windows.c
+++ windows.c
@@ -62,41 +62,6 @@ static int buffer(lua_State *L) {
     return 1;
 }
 
-static int pointer(lua_State *L) {
-    // int* not int, no dereference on NULL check
-    int *ptr = (int *)malloc(sizeof(int));
-    if (ptr == NULL) {
-        return luaL_error(L, "malloc failed");
-    }
-
-    printf("enter required number: ");
-    scanf("%d", ptr);
-
-    // push the pointer so Lua can hold and return it
-    lua_pushlightuserdata(L, ptr);
-    return 1;
-}
-
-static int freePointer(lua_State *L) {
-    if (lua_type(L, 1) != LUA_TLIGHTUSERDATA) {
-        return luaL_error(L, "expected pointer, got %s",
-            lua_typename(L, lua_type(L, 1)));
-    }
-
-    // lua_touserdata reads, cast to int* not int
-    int *ptr = (int *)lua_touserdata(L, 1);
-
-    if (ptr == NULL) {
-        return luaL_error(L, "pointer is NULL");
-    }
-
-    free(ptr);
-    ptr = NULL;
-
-    lua_pushboolean(L, 1); // semicolon
-    return 1;
-}
-
 // heaps here!
 
 static int createHeap(lua_State *L) {
@@ -406,8 +371,6 @@ static const luaL_Reg lib[] = {
     { "virtualFree", virtualFree },
     { "createProcess", createProcess },
     { "closeHandle", closeHandle},
-    { "pointer", pointer },
-    { "freePointer", freePointer},
     {NULL, NULL}
 };
 
